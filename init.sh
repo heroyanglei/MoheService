44e912184918c63cb8901b4343d044d5e2e8cea1
jay <547671777@qq.com>
2018-10-28 21:47:30 +0800

    Add files via upload
    环境变量

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export CONFIG_SERVICE_PASSWORD=root
+export MONGODB_PASSWORD=root
+export ACCOUNT_SERVICE_PASSWORD=root
+export STATISTICS_SERVICE_PASSWORD=root
+export NOTIFICATION_SERVICE_PASSWORD=root
+
