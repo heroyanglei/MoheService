7f5e2f365ba924ba96ceb95bbb6df044744bac1b
kang wang <>
2018-10-31 12:28:04 +0800

    mybatis测试代码提交

---
+++ mybatis-service/src/main/java/com/piggymetrics/mybatis/mysql/SpringDataMybatisSamplesApplication.java
@@ -0,0 +1,10 @@
+package com.piggymetrics.mybatis.mysql;
+
+/**
+ * @ClassName SpringDataMybatisSamplesApplication
+ * @Description TODO
+ * @Author kangwang
+ * @Date 9:16 AM 2018/10/30
+ */
+public class SpringDataMybatisSamplesApplication {
+}
